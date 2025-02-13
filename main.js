143f33b30dbc52229cd95a34fe89a5e42d86cecf
SBraden04 <w0485610@campus.nscc.ca>
2025-02-13 11:51:32 -0400

    My valentines day card

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+function yuhHuh() {
+    let img = document.querySelector("img");
+    img.src = "yippee-iluvmybf.gif"
+    
+   
+    }
+   function nuhUh() {
+    let img = document.querySelector("img");
+    img.src = "sideeye.jpeg"
+
+    button = document.querySelector(".no");
+    button.remove();
+
+    button2 = document.querySelector(".yes");
+    button2.innerText = "YUH HUH >:)"
+    button2.style.width = "170px"
+    button2.style.height = "50px"
+    button2.style.fontSize = "20px"
+    
+    }
